2bca1aa25904512c4c3a766e7a549792c8c9d41a
valasatava <yana.valasatava@gmail.com>
2022-07-29 18:41:49 -0700

    handle zero scores

--- src/ui/view/scores.tsx
+++ src/ui/view/scores.tsx
@@ -21,6 +21,10 @@ function scores(alignment: Alignment) {
     return { rmsd, tmscore, simscore, identity, similarity, length, coverage };
 }
 
+function defined(num: number | number[] | undefined) {
+    return num !== undefined;
+}
+
 export function AlignmentScoresComponent(props: { ctx: ApplicationContext }) {
 
     const subs: Subscription[] = [];
@@ -38,24 +42,24 @@ export function AlignmentScoresComponent(props: { ctx: ApplicationContext }) {
             <table className='tbl-scores'>
                 <thead>
                     <tr key={0}>
-                        {data.rmsd && <th>RMSD</th>}
-                        {data.tmscore && <th>TM-score</th>}
-                        {data.simscore && <th>Score</th>}
-                        {data.identity && <th>SI%</th>}
-                        {data.similarity && <th>SS%</th>}
-                        {data.length && <th>Length</th>}
-                        {data.coverage && <th>Coverage</th>}
+                        {defined(data.rmsd) && <th>RMSD</th>}
+                        {defined(data.tmscore) && <th>TM-score</th>}
+                        {defined(data.simscore) && <th>Score</th>}
+                        {defined(data.identity) && <th>SI%</th>}
+                        {defined(data.similarity) && <th>SS%</th>}
+                        {defined(data.length) && <th>Length</th>}
+                        {defined(data.coverage) && <th>Coverage</th>}
                     </tr>
                 </thead>
                 <tbody>
                     <tr key={1}>
-                        {data.rmsd && <td>{round(data.rmsd)}</td>}
-                        {data.tmscore && <td>{round(data.tmscore)}</td>}
-                        {data.simscore && <td>{round(data.simscore)}</td>}
-                        {data.identity && <td>{round(data.identity * 100)}</td>}
-                        {data.similarity && <td>{round(data.similarity * 100)}</td>}
-                        {data.length && <td>{data.length}</td>}
-                        {data.coverage && <td>{data.coverage[0]}% / {data.coverage[1]}%</td>}
+                        {defined(data.rmsd) && <td>{round(data.rmsd!)}</td>}
+                        {defined(data.tmscore) && <td>{round(data.tmscore!)}</td>}
+                        {defined(data.simscore) && <td>{round(data.simscore!)}</td>}
+                        {defined(data.identity) && <td>{round(data.identity! * 100)}</td>}
+                        {defined(data.similarity) && <td>{round(data.similarity! * 100)}</td>}
+                        {defined(data.length) && <td>{data.length}</td>}
+                        {defined(data.coverage) && <td>{data.coverage![0]}% / {data.coverage![1]}%</td>}
                     </tr>
                 </tbody>
             </table>
